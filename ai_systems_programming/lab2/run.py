489b5bdd51f5f8030cd6f81c3fedbac1f40c8738
AnnaYasenova <yasenova2013@gmail.com>
2019-11-12 23:40:00 +0200

    lab2 added

---
+++ ai_systems_programming/lab2/run.py
@@ -0,0 +1,214 @@
+import timeit
+from search import *
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
+from PIL import Image
+import matplotlib.pyplot as plt
+
+
+class Problem(object):
+
+    """The abstract class for a formal problem. You should subclass
+    this and implement the methods actions and result, and possibly
+    __init__, goal_test, and path_cost. Then you will create instances
+    of your subclass and solve them with the various search functions."""
+
+    def __init__(self, initial, goal=None):
+        """The constructor specifies the initial state, and possibly a goal
+        state, if there is a unique goal. Your subclass's constructor can add
+        other arguments."""
+        self.initial = initial
+        self.goal = goal
+
+    def actions(self, state):
+        """Return the actions that can be executed in the given
+        state. The result would typically be a list, but if there are
+        many actions, consider yielding them one at a time in an
+        iterator, rather than building them all at once."""
+        raise NotImplementedError
+
+    def result(self, state, action):
+        """Return the state that results from executing the given
+        action in the given state. The action must be one of
+        self.actions(state)."""
+        raise NotImplementedError
+
+    def goal_test(self, state):
+        """Return True if the state is a goal. The default method compares the
+        state to self.goal or checks for state in self.goal if it is a
+        list, as specified in the constructor. Override this method if
+        checking against a single self.goal is not enough."""
+        if isinstance(self.goal, list):
+            return is_in(state, self.goal)
+        else:
+            return state == self.goal
+
+    def path_cost(self, c, state1, action, state2):
+        """Return the cost of a solution path that arrives at state2 from
+        state1 via action, assuming cost c to get up to state1. If the problem
+        is such that the path doesn't matter, this function will only look at
+        state2.  If the path does matter, it will consider c and maybe state1
+        and action. The default method costs 1 for every step in the path."""
+        return c + 1
+
+    def value(self, state):
+        """For optimization problems, each state has a value.  Hill-climbing
+        and related algorithms try to maximize this value."""
+        raise NotImplementedError
+
+
+class Node:
+    """A node in a search tree. Contains a pointer to the parent (the node
+    that this is a successor of) and to the actual state for this node. Note
+    that if a state is arrived at by two paths, then there are two nodes with
+    the same state.  Also includes the action that got us to this state, and
+    the total path_cost (also known as g) to reach the node.  Other functions
+    may add an f and h value; see best_first_graph_search and astar_search for
+    an explanation of how the f and h values are handled. You will not need to
+    subclass this class."""
+
+    def __init__(self, state, parent=None, action=None, path_cost=0):
+        """Create a search tree Node, derived from a parent by an action."""
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.path_cost = path_cost
+        self.depth = 0
+        if parent:
+            self.depth = parent.depth + 1
+
+    def __repr__(self):
+        return "<Node {}>".format(self.state)
+
+    def __lt__(self, node):
+        return self.state < node.state
+
+    def expand(self, problem):
+        """List the nodes reachable in one step from this node."""
+        return [self.child_node(problem, action)
+                for action in problem.actions(self.state)]
+
+    def child_node(self, problem, action):
+        """[Figure 3.10]"""
+        next_state = problem.result(self.state, action)
+        next_node = Node(next_state, self, action,
+                         problem.path_cost(self.path_cost, self.state,
+                                           action, next_state))
+        return next_node
+
+    def solution(self):
+        """Return the sequence of actions to go from the root to this node."""
+        return [node.action for node in self.path()[1:]]
+
+    def path(self):
+        """Return a list of nodes forming the path from the root to this node."""
+        node, path_back = self, []
+        while node:
+            path_back.append(node)
+            node = node.parent
+        return list(reversed(path_back))
+
+    # We want for a queue of nodes in breadth_first_graph_search or
+    # astar_search to have no duplicated states, so we treat nodes
+    # with the same state as equal. [Problem: this may not be what you
+    # want in other contexts.]
+
+    def __eq__(self, other):
+        return isinstance(other, Node) and self.state == other.state
+
+    def __hash__(self):
+        return hash(self.state)
+
+
+class GraphProblem(Problem):
+
+    """The problem of searching a graph from one node to another."""
+
+    def __init__(self, initial, goal, graph):
+        Problem.__init__(self, initial, goal)
+        self.graph = graph
+
+    def actions(self, A):
+        """The actions at a graph node are just its neighbors."""
+        return list(self.graph.get(A).keys())
+
+    def result(self, state, action):
+        """The result of going to a neighbor is just that neighbor."""
+        return action
+
+    def path_cost(self, cost_so_far, A, action, B):
+        return cost_so_far + (self.graph.get(A, B) or infinity)
+
+    def find_min_edge(self):
+        """Find minimum value of edges."""
+        m = infinity
+        for d in self.graph.graph_dict.values():
+            local_min = min(d.values())
+            m = min(m, local_min)
+
+        return m
+
+    def h(self, node):
+        """h function is straight-line distance from a node's state to goal."""
+        locs = getattr(self.graph, 'locations', None)
+        if locs:
+            if type(node) is str:
+                return int(distance(locs[node], locs[self.goal]))
+
+            return int(distance(locs[node.state], locs[self.goal]))
+        else:
+            return infinity
+
+
+def plot_NQueens(solution):
+    n = len(solution)
+    board = np.array([2 * int((i + j) % 2) for j in range(n) for i in range(n)]).reshape((n, n))
+    im = Image.open('queen_s.png')
+    height = im.size[1]
+    im = np.array(im).astype(np.float) / 255
+    fig = plt.figure(figsize=(7, 7))
+    ax = fig.add_subplot(111)
+    ax.set_title('{} Queens'.format(n))
+    plt.imshow(board, cmap='binary', interpolation='nearest')
+    # NQueensCSP gives a solution as a dictionary
+    if isinstance(solution, dict):
+        for (k, v) in solution.items():
+            newax = fig.add_axes([0.064 + (k * 0.112), 0.062 + ((7 - v) * 0.112), 0.1, 0.1], zorder=1)
+            newax.imshow(im)
+            newax.axis('off')
+    # NQueensProblem gives a solution as a list
+    elif isinstance(solution, list):
+        for (k, v) in enumerate(solution):
+            newax = fig.add_axes([0.064 + (k * 0.112), 0.062 + ((7 - v) * 0.112), 0.1, 0.1], zorder=1)
+            newax.imshow(im)
+            newax.axis('off')
+    fig.tight_layout()
+    plt.show()
+
+
+nqp = NQueensProblem(8)
+
+#breadth_first_tree_search(nqp)
+
+def func(nqp):
+    #breadth_first_tree_search(nqp)
+    bfts = breadth_first_tree_search(nqp).solution()
+    #bfts = breadth_first_graph_search(nqp).solution()
+    #print(breadth_first_tree_search(nqp))
+    #plot_NQueens(bfts)
+
+print('breadth_first_tree_search:', timeit.timeit('[func(nqp)]', globals=globals(), number=500))
+
+nqp = NQueensProblem(8)
+
+def func(nqp):
+    astar = astar_search(nqp).solution()
+
+print('astar_search:', timeit.timeit('[func(nqp)]', globals=globals(), number=500))
+
+nqp = NQueensProblem(8)
+
+astar = breadth_first_graph_search(nqp).solution()
+
+plot_NQueens(astar)
